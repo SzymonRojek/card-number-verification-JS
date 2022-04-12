efd56b5b76c68801e022c0ffc861c4e64a0894e8
Szymon Rojek <sz.rojek@gmail.com>
2022-04-12 20:14:28 +0100

    add exports index

---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+export { default as isLuhnOK } from "./isLuhnOK.js";
+export { default as isStartingNumberCorrect } from "./isStartingNumberCorrect";
+export { default as throwWhenNotString } from "./throwWhenNotString";
+export { default as hasOnlyNumbersAndSpaces } from "./hasOnlyNumbersAndSpaces";
+export { default as hasCardProperLength } from "./hasCardProperLength";
