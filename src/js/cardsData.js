ce1ec8d9d2221a0ec5b3e54330464641a2c52047
Szymon Rojek <sz.rojek@gmail.com>
2022-04-25 23:37:03 +0100

    create cardsData

---
+++ src/js/cardsData.js
@@ -0,0 +1,19 @@
+const cardsData = [
+  {
+    name: "Mastercard",
+    startingNumbers: [51, 52, 53, 54, 55],
+    length: [16],
+  },
+  {
+    name: "Visa",
+    startingNumbers: [4],
+    length: [13, 16],
+  },
+  {
+    name: "American Express",
+    startingNumbers: [34, 37],
+    length: [15],
+  },
+];
+
+export default cardsData;
