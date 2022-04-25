c66147ae8a9752b02a449745462e7d0ebd6a2939
Szymon Rojek <sz.rojek@gmail.com>
2022-04-25 23:49:22 +0100

    edit sliceDondition, make it shorter

--- src/helpers/isStartingNumberCorrect.js
+++ src/helpers/isStartingNumberCorrect.js
@@ -1,6 +1,6 @@
 export default function isStartingNumberCorrect(card, data) {
-  const sliceCondition = card.length === 1 ? 1 : 2;
-  const slicedNumber = +data.slice(0, sliceCondition).join("");
+  const sliceCondition = card.length ? 1 : 2;
+  const slicedNumber = Number(data.slice(0, sliceCondition).join(""));
 
   return card.includes(slicedNumber);
 }
