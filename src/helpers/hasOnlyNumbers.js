7bd01c6511c418d76b4eca4314e1d036790fa669
Szymon Rojek <sz.rojek@gmail.com>
2022-04-25 23:48:57 +0100

    rename parameter

--- src/helpers/hasOnlyNumbers.js
+++ src/helpers/hasOnlyNumbers.js
@@ -1,5 +1,5 @@
 const errorMessage = { message: "Password has to contain only numbers!" };
 
-export default function hasOnlyNumbers(str) {
-  return /^\d+$/.test(str) || errorMessage;
+export default function hasOnlyNumbers(inputValue) {
+  return /^\d+$/.test(inputValue) || errorMessage;
 }
--- src/helpers/isLuhnOK.js
+++ src/helpers/isLuhnOK.js
@@ -1,11 +1,11 @@
 const errorMessage = {
-  message: "Probably you have typed wrong numbers - check them again!",
+  message: "Luhn's algorithm does not point to the truth!",
 };
 
-export default function isLuhnOK(str) {
+export default function isLuhnOK(inputValue) {
   const luhnNumbers = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
   const numberPatternReg = /\d+/g;
-  const onlyNumbers = str.match(numberPatternReg) || [];
+  const onlyNumbers = inputValue.match(numberPatternReg) || [];
   const arrNumbers = onlyNumbers
     .join("")
     .split("")
@@ -13,7 +13,7 @@ export default function isLuhnOK(str) {
 
   return (
     arrNumbers.reduceRight((acc, _, i) => {
-      const AUX_NUM = parseInt(str.charAt(i), 10);
+      const AUX_NUM = parseInt(inputValue.charAt(i), 10);
 
       return (acc += i % 2 === 0 ? AUX_NUM : luhnNumbers[AUX_NUM]);
     }, 0) %
