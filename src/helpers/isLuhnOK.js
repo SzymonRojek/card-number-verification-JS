90d522f7b70a3e68d499232b8e842ab11abde8fa
Szymon Rojek <sz.rojek@gmail.com>
2022-04-29 21:53:55 +0100

    refactor fn

--- src/helpers/isLuhnOK.js
+++ src/helpers/isLuhnOK.js
@@ -3,21 +3,21 @@ const errorMessage = {
 };
 
 export default function isLuhnOK(inputValue) {
-  const luhnNumbers = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
-  const numberPatternReg = /\d+/g;
-  const onlyNumbers = inputValue.match(numberPatternReg) || [];
-  const arrNumbers = onlyNumbers
-    .join("")
-    .split("")
-    .map((el) => Number(el));
+  const digitsArrayCollections = [...inputValue].map((num) => Number(num));
 
   return (
-    arrNumbers.reduceRight((acc, _, i) => {
-      const AUX_NUM = parseInt(inputValue.charAt(i), 10);
-
-      return (acc += i % 2 === 0 ? AUX_NUM : luhnNumbers[AUX_NUM]);
-    }, 0) %
+    digitsArrayCollections
+      .reverse()
+      .map((num, i) => (i % 2 === 1 ? num * 2 : num))
+      .reduce((total, num) => total + (num > 9 ? sumDigits(num) : num), 0) %
       10 ===
       0 || errorMessage
   );
+}
+
+function sumDigits(num) {
+  return String(num)
+    .split("")
+    .map((num) => Number(num))
+    .reduce((total, num) => total + num, 0);
 }
