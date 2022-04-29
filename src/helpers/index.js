eddf362ec62177232e8fbddd654395daf04943b0
Szymon Rojek <sz.rojek@gmail.com>
2022-04-29 21:53:32 +0100

    add new exports

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,3 +4,5 @@ export { default as throwWhenNotString } from "./throwWhenNotString";
 export { default as hasOnlyNumbers } from "./hasOnlyNumbers";
 export { default as hasCardProperLength } from "./hasCardProperLength";
 export { default as getSingleOrDoubleDigits } from "./getSingleOrDoubleDigits";
+export { default as formatCardNumber } from "./formatCardNumber";
+export { default as displayFormatNumbersOnChange } from "./displayFormatNumberOnChange";
