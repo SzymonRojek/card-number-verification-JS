ba5a90d81b44f807a279f6cd98530823aaf9125c
Szymon Rojek <sz.rojek@gmail.com>
2022-05-05 22:19:48 +0100

    edit keys

--- src/js/cardsData.js
+++ src/js/cardsData.js
@@ -2,23 +2,23 @@ export const cardsData = [
   {
     imgUrl:
       "https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg",
+    imgClass: "card__logo--mastercard",
     startingNumbers: [51, 52, 53, 54, 55],
     length: [16],
-    atr: "card__logo--mastercard",
   },
   {
     imgUrl:
       "https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg",
+    imgClass: "card__logo--visa",
     startingNumbers: [4],
     length: [13, 16],
-    atr: "card__logo--visa",
   },
   {
     imgUrl:
       "https://remote-europe.com/sites/default/files/styles/medium_300x300_/public/2020-11/americanexpress.jpg?itok=i_4MMKM0",
+    imgClass: "card__logo--americanExpress",
     startingNumbers: [34, 37],
     length: [15],
-    atr: "card__logo--americanExpress",
   },
 ];
 
