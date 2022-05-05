1ce298abb89d1479cff456e1201aaca004e1982d
Szymon Rojek <sz.rojek@gmail.com>
2022-05-05 22:19:07 +0100

    change exports

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,5 @@
 export { default as isLuhnOK } from "./isLuhnOK.js";
-export { default as isStartingNumberCorrect } from "./isStartingNumberCorrect";
 export { default as throwWhenNotString } from "./throwWhenNotString";
-export { default as hasOnlyNumbers } from "./hasOnlyNumbers";
-export { default as hasCardProperLength } from "./hasCardProperLength";
-export { default as getSingleOrDoubleDigits } from "./getSingleOrDoubleDigits";
+export { default as hasProperLength } from "./hasProperLength";
+export { default as findCardWithStartingNumber } from "./findCardWithStartingNumber";
 export { default as formatCardNumber } from "./formatCardNumber";
-export { default as displayFormatNumbersOnChange } from "./displayFormatNumberOnChange";
